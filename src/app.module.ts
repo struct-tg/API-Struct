2505147c6cb1c4c188a812c048b05fa6810ca448
Anderson Correia <anderson_souza33@outlook.com.br>
2023-08-19 12:20:29 -0300

    feat: estrutura do módulo de task

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { UserModule } from './user/user.module';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConfig } from './configs';
 import { AuthModule } from './guards/auth/auth.module';
+import { TaskModule } from './task/task.module';
 
 @Module({
   imports: [
     JwtModule.register(jwtConfig),
     UserModule,
-    AuthModule
+    AuthModule,
+    TaskModule
   ],
   controllers: [],
   providers: [],
