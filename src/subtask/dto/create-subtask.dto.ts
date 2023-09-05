8e9cde9cf3b888e5f02d3d6dc09c14d5c66f4f64
Anderson Correia <anderson_souza33@outlook.com.br>
2023-09-05 20:23:00 -0300

    feat: dtos

--- src/subtask/dto/create-subtask.dto.ts
+++ src/subtask/dto/create-subtask.dto.ts
@@ -1,1 +1,12 @@
-export class CreateSubtaskDto {}
+import { IsNotEmpty, IsNumber, IsString, MinLength } from "class-validator";
+
+export class CreateSubtaskDto {
+    @IsNotEmpty({message: `description não pode ser vazio`})
+    @IsString({message: `description deve ser uma string`})
+    @MinLength(2, {message: `description deve ter um tamanho maior ou igual a 2 caracteres`})
+    description: string;
+
+    @IsNotEmpty({message: `description não pode ser vazio`})
+    @IsNumber({},{message: `taskId deve ser um número`})
+    taskId: number;
+}
--- src/subtask/dto/update-subtask.dto.ts
+++ src/subtask/dto/update-subtask.dto.ts
@@ -1,4 +1,9 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateSubtaskDto } from './create-subtask.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateSubtaskDto extends PartialType(CreateSubtaskDto) {}
+export class UpdateSubtaskDto extends PartialType(CreateSubtaskDto) {
+    @IsOptional()
+    @IsBoolean({message: `Deve ser um booleano`})
+    status: boolean
+}
