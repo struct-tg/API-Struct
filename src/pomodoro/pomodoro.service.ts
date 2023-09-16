de66c682eda97e11e8c22c0a2ecc79097d306d36
Anderson Correia <anderson_souza33@outlook.com.br>
2023-09-16 19:28:06 -0300

    refactor: formatação padronização do findAll do pomodoro

--- src/pomodoro/pomodoro.service.ts
+++ src/pomodoro/pomodoro.service.ts
@@ -2,6 +2,8 @@ import { Inject, Injectable } from '@nestjs/common';
 import { CreatePomodoroDto } from './dto/create-pomodoro.dto';
 import { UpdatePomodoroDto } from './dto/update-pomodoro.dto';
 import { PomodoroGatewayInterface } from './gateways/pomodoro-bd/pomodoro-gateway-interface';
+import { Pagination } from 'src/utils/pagination';
+import { Pomodoro } from './entities/pomodoro.entity';
 
 @Injectable()
 export class PomodoroService {
@@ -20,8 +22,8 @@ export class PomodoroService {
   }
 
   async findAll(idUserLog: number) {
-    const listPomodoro = await this.pomodoroGateway.findAll(idUserLog);
-    return listPomodoro;
+    const data = await this.pomodoroGateway.findAll(idUserLog);
+    return new Pagination<Pomodoro>(data, 1, data.length, data.length);
   }
 
   findOne(id: number) {
